a70a15701a6de6cea85f65e56195d81a3f49a346
tonyishangu <tonyishangu@gmail.com>
2021-07-30 11:42:01 +0300

    footer logo

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const footer = () => {
     return (
@@ -9,3 +10,15 @@ const footer = () => {
 }
 
 export default footer
+
+const H1 = styled.h1`
+  font-family: cursive;
+  color: #37474f;
+  padding-left: 10px;
+  font-size: 35px;
+  ;
+  @media (max-width: 576px) {
+    padding-left: 3px;
+    font-size: 20px;
+  }
+`;
