220a5f552e2f3e1233d752d58a5e61f2d8f8c104
tonyishangu <tonyishangu@gmail.com>
2021-07-30 11:42:50 +0300

    footer container

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -11,6 +11,32 @@ const footer = () => {
 
 export default footer
 
+const HeroContainer = styled.div`
+  width: 100%;
+  max-width: 100%;
+  min-height: 64vh;
+  background-color: #0f1636;
+  /* color: #000; */
+  /* padding: 0; */
+  height: 100%;
+  padding-left: 50px;
+  padding-right: 50px;
+  display: grid;
+  grid-gap: 1rem;
+  align-items: center;
+  justify-content: center;
+  grid-template-columns: 1fr;
+  @media screen and (min-width: 960px) {
+    grid-template-columns: 1fr 1fr;
+    padding-left: 5px;
+    padding-right: 5px;
+  }
+  @media screen and (max-width: 600px) {
+    padding-left: 2px;
+    padding-right: 2px;
+  }
+`;
+
 const H1 = styled.h1`
   font-family: cursive;
   color: #37474f;
